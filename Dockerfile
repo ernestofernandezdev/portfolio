8fbefcf31c31bda25a9ccdacf81df275b7091297
Ernesto Fernandez <fernandez.ernes@gmail.com>
2025-02-19 14:36:34 -0300

    Se agregó un endpoint test en la api

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM nginx:latest
 
 # Copia los archivos estáticos al directorio adecuado en NGINX
 COPY ./static /usr/share/nginx/html
+COPY ./nginx /etc/nginx/nginx.conf
 
 # Exponer el puerto 80 (por defecto de NGINX)
 EXPOSE 80
--- portfolio-api/src/main/java/com/ernestofernandezdev/portfolio_api/WebhookController.java
+++ portfolio-api/src/main/java/com/ernestofernandezdev/portfolio_api/WebhookController.java
@@ -3,6 +3,7 @@ package com.ernestofernandezdev.portfolio_api;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class WebhookController {
             e.printStackTrace();
             return ResponseEntity.status(500).body("Error al procesar el webhook: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Funciona!");
     }
 }
