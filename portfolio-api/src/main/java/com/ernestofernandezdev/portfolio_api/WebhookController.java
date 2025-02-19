0c4d226a0fc1cedaec1fadd59ed5881b85cb10f4
Ernesto Fernandez <fernandez.ernes@gmail.com>
2025-02-19 11:08:11 -0300

    Se agrego una app en Spring para autodeployar la aplicacion

---
+++ portfolio-api/src/main/java/com/ernestofernandezdev/portfolio_api/WebhookController.java
@@ -0,0 +1,37 @@
+package com.ernestofernandezdev.portfolio_api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/api")
+public class WebhookController {
+    @PostMapping("/deploy")
+    public ResponseEntity<String> deploy(@RequestBody JsonNode payload) {
+        try {
+            String imageName = payload.at("/push_data/repository/name").asText();
+            String tag = payload.at("/push_data/tag").asText();
+
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "bash", "-c", String.format("/opt/deploy.sh %s %s", imageName, tag)
+            );
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return ResponseEntity.ok("Imagen actualizada con éxito");
+            } else {
+                return ResponseEntity.status(500).body("Error al ejecutar el script de actualización");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Error al procesar el webhook: " + e.getMessage());
+        }
+    }
+}
